7f8a87aba831d5b034ca58768254d89340f27c09
kayky.santos <kayky.santos@DESKTOP-PPF77M2>
2026-01-11 11:58:19 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScreenSound
+{
+    class Musica
+    {
+        public string Artista { get; set; }
+        public string Nome { get; set; }
+        public int Duracao { get; set; }
+        public bool Disponivel { get; set; }
+
+        public Musica(string artista, string nome, int duracao, bool disponivel)
+        {
+            this.Artista = artista;
+            this.Nome = nome;
+            this.Duracao = duracao;
+            this.Disponivel = disponivel;
+        }
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Artista: {Artista}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Duração: {Duracao} segundos");
+            Console.WriteLine($"Disponível: {(Disponivel ? "Sim" : "Não")}\n");
+        }
+    }
+}
+
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSound;
+
+Musica musica = new Musica(
+    "Pink Floyd",
+    "Comfortably Numb",
+    384,
+    true);
+
+Musica musica2 = new Musica(
+   "Time",
+   "Pink Floyd",
+    413,
+    false);
+
+
+musica.ExibirFichaTecnica();
+
+musica2.ExibirFichaTecnica();
+
