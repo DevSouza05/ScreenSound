80d7f27926bdb380a59d6614cf185f1d3f805436
peters2on moraes <peterson.moraes2005@gmail.com>
2026-01-11 22:04:07 -0300

    feat: create a new class Music

--- ScreenSound/Music.cs
+++ ScreenSound/Music.cs
@@ -4,27 +4,34 @@ using System.Text;
 
 namespace ScreenSound
 {
-    class Musica
+    class Music
     {
         public string Artista { get; set; }
         public string Nome { get; set; }
         public int Duracao { get; set; }
         public bool Disponivel { get; set; }
 
-        public Musica(string artista, string nome, int duracao, bool disponivel)
+        public string DescriacaoResumida => $"A banda {Nome} Pertence ao {Artista}";
+
+
+        public Music(string artista, string nome, int duracao ,bool disponivel)
         {
             this.Artista = artista;
             this.Nome = nome;
             this.Duracao = duracao;
             this.Disponivel = disponivel;
+           
         }
 
         public void ExibirFichaTecnica()
         {
+            
             Console.WriteLine($"Artista: {Artista}");
             Console.WriteLine($"Nome: {Nome}");
             Console.WriteLine($"Duração: {Duracao} segundos");
             Console.WriteLine($"Disponível: {(Disponivel ? "Sim" : "Não")}\n");
+            Console.WriteLine($"Descrição Resumida: {DescriacaoResumida}\n");
+           
         }
     }
 }
